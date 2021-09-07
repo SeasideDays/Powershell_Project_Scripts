3cfc365b6425008672f868fffeb7286b6250dec1
SeasideDays <88351407+SeasideDays@users.noreply.github.com>
2021-09-07 17:21:11 -0400

    Delete Disable_accts_greater than_30 days.ps1

--- Disable_accts_greater than_30 days.ps1
+++
@@ -1,3 +0,0 @@
-﻿Get-ADUser -Filter *  -Properties Name,Lastlogontimestamp,PasswordNeverExpires | Where-Object {([datetime]::FromFileTime($_.lastlogontimestamp) -le (Get-Date).adddays(-30)) -and ($_.passwordNeverExpires -ne "true") } | Disable-ADAccount
-
-
