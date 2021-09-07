642ed6b4cfa06cd6d7e27653a2225c72246f7209
SeasideDays <88351407+SeasideDays@users.noreply.github.com>
2021-09-07 17:21:54 -0400

    Delete Saving Credentials.ps1

--- Saving Credentials.ps1
+++
@@ -1,9 +0,0 @@
-﻿Get-Credential "admin@ABusiness.Com" |
-
-Export-Clixml C:\ABusinessCredential.xml
-
-$cred = Import-Clixml C:\ABusinessCredential.xml
-
-Connect-MsolService -Credential $cred
-
-Msol can be whatever service you are connecting to
