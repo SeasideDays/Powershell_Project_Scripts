213412673bce30661afcf11c1f22b2caeec1a1fe
SeasideDays <88351407+SeasideDays@users.noreply.github.com>
2021-09-07 17:20:43 -0400

    Delete Account_Expiration_Date.ps1

--- Account_Expiration_Date.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-ADUser -Filter 'enabled -eq $true' -Properties AccountExpirationDate | Select sAMAccountName, distinguishedName, AccountExpirationDate
-
-Get-ADUser -Filter 'enabled -eq $true' -Searchbase "OU=IT,DC=Adatum,DC=com" -Properties AccountExpirationDate | Select SAMAccountName, distinguishedName, AccountExpirationDate |export-csv C:\ExpiryDate.csv -NoTypeInformation
-
-Search-ADAccount -AccountExpired | Where-Object {$_.AccountExpirationDate -ge ((Get-Date).AddDays(-1))}
